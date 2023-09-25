912d255b3ff9bc8d32175d86af40ec21f580a85b
sebastianpiresmolin <piresmolinsecure@gmail.com>
2023-09-25 14:14:36 +0200

    html/css built

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const mathArr = [];
+
+const button1 = document.getElementById("n1btn");
+
+const buttonPressed = e => {
+    console.log(e.target.id);
+}
+
+for (let buttons in button1) {
+    button.addEventListener("click", buttonPressed);
+}
