a7e4e1bc3e872552bd6a9aaccf645fa852bd66fc
sebastianpiresmolin <piresmolinsecure@gmail.com>
2023-10-11 22:58:23 +0200

    added numpad functionality

--- main.js
+++ main.js
@@ -1,10 +1,59 @@
+let totalString = ''; //displayed on screen
 
-const button1 = document.getElementById("n1btn");
+const screenElement = document.getElementById('screen');
+//buttons
+const one = document.getElementById('n1');
+const two = document.getElementById('n2');
+const three = document.getElementById('n3');
+const four = document.getElementById('n4');
+const five = document.getElementById('n5');
+const six = document.getElementById('n6');
+const seven = document.getElementById('n7');
+const eight = document.getElementById('n8');
+const nine = document.getElementById('n9');
 
-const buttonPressed = e => {
-    console.log(e.target.id);
-}
+//number pad eventListeners
+one.addEventListener("click", function() {
+    totalString = totalString.concat('1');
+    screenElement.innerHTML = totalString;
+});
 
-for (let buttons in button1) {
-    button.addEventListener("click", buttonPressed);
-}
+two.addEventListener("click", function() {
+    totalString = totalString.concat('2');
+    screenElement.innerHTML = totalString;
+});
+
+three.addEventListener("click", function() {
+    totalString = totalString.concat('3');
+    screenElement.innerHTML = totalString;
+});
+
+four.addEventListener("click", function() {
+    totalString = totalString.concat('4');
+    screenElement.innerHTML = totalString;
+});
+
+five.addEventListener("click", function() {
+    totalString = totalString.concat('5');
+    screenElement.innerHTML = totalString;
+});
+
+six.addEventListener("click", function() {
+    totalString = totalString.concat('6');
+    screenElement.innerHTML = totalString;
+});
+
+seven.addEventListener("click", function() {
+    totalString = totalString.concat('7');
+    screenElement.innerHTML = totalString;
+});
+
+eight.addEventListener("click", function() {
+    totalString = totalString.concat('8');
+    screenElement.innerHTML = totalString;
+});
+
+nine.addEventListener("click", function() {
+    totalString = totalString.concat('9');
+    screenElement.innerHTML = totalString;
+});
